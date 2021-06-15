cba16a7fc63f4f3a14b8b13dcf577125e1c16884
iuliaaa2110 <iulia_andreea9@yahoo.com>
2021-06-15 16:52:38 +0300

    first change of Credits.xml

--- app/src/main/java/ro/unibuc/votingapp/presentation/view/CreditsActivity.java
+++ app/src/main/java/ro/unibuc/votingapp/presentation/view/CreditsActivity.java
@@ -15,34 +15,6 @@ public final class CreditsActivity extends Activity {
     private final static String github = "https://github.com/ReksioCroft/";
     private final static String google = "https://events.withgoogle.com/atelierul-digital-pentru-programatori/";
 
-
-    @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_credits );
-        VotingApplication.getApplication().addActivity( this );
-
-        CardView view = findViewById( R.id.cardViewProgrammer );
-        view.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick( View v ) {
-                Intent intent = new Intent( Intent.ACTION_VIEW, Uri.parse( github ) );
-                intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
-                getApplicationContext().startActivity( intent );
-            }
-        } );
-
-        view = findViewById( R.id.cardViewCredits );
-        view.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick( View v ) {
-                Intent intent = new Intent( Intent.ACTION_VIEW, Uri.parse( google ) );
-                intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
-                getApplicationContext().startActivity( intent );
-            }
-        } );
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
